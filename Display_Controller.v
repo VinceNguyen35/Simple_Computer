57c6ab67a6942a3e5449c80c9f8ac93585761021
Vinnydaninja <44877059+Vinnydaninja@users.noreply.github.com>
2020-01-25 01:33:39 -0800

    Update Display_Controller.v

--- Display_Controller.v
+++ Display_Controller.v
@@ -2,14 +2,13 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 // File Name: 	Display_Controller.v
-// Project:		CECS 301 Lab 8: 16-bit RISC Processor
-//	Student:		Vince Nguyen & Matthew Buchholz
-//	Rev. Date:	Mar. 20, 2019
+// Authors:	Vince Nguyen & Matthew Buchholz
+// Rev. Date:	Mar. 20, 2019
 //
-//	Purpose:		This is the display controller that outputs onto the anodes.
+// Purpose:		This is the display controller that outputs onto the anodes.
 //
-//	Notes:		The module uses a 480Hz clock, a pixel controller, a 8 to 1 mux,
-//					and a hex to 7 segment display.
+// Notes:		The module uses a 480Hz clock, a pixel controller,
+//			an 8 to 1 mux, and a hex to 7 segment display.
 //
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -23,10 +22,10 @@ module Display_Controller(
     
     wire       pixel_clk_out;
     wire [2:0] seg_sel;
-	 wire [3:0]	hex_sel;
+    wire [3:0] hex_sel;
     
-    pixel_clk		 	u0(clk, 				rst, 	   pixel_clk_out);
-	 pixel_controller u1(pixel_clk_out, rst, A,  seg_sel);
-	 ad_mux		     	u2(seg, 				seg_sel, hex_sel);
-    hex_to_7seg		u3(pixel_clk_out, rst, 		hex_sel, hex);
+    pixel_clk		u0(clk, rst, pixel_clk_out);
+    pixel_controller 	u1(pixel_clk_out, rst, A, seg_sel);
+    ad_mux		u2(seg, seg_sel, hex_sel);
+    hex_to_7seg		u3(pixel_clk_out, rst, hex_sel, hex);
 endmodule

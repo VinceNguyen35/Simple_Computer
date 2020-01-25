2ff87ab229c3f977d9fefbb3d89e6c43e42ae522
Vinnydaninja <44877059+Vinnydaninja@users.noreply.github.com>
2020-01-25 01:30:38 -0800

    Update CPU_EU.v

--- CPU_EU.v
+++ CPU_EU.v
@@ -2,53 +2,52 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 // File Name: 	CPU_EU.v
-// Project:		CECS 301 Lab 8: 16-bit RISC Processor
-//	Student:		Vince Nguyen & Matthew Buchholz
-//	Rev. Date:	May 8, 2019
+// Authors:	Vince Nguyen & Matthew Buchholz
+// Rev. Date:	May 8, 2019
 //
-//	Purpose:		This is the CPU Execution Unit.  The module utilizes an integer
-//					datapath to execute instructions and write out to memory.  The 
-//					module outputs a 16-bit address location and 16 bits of data out, 
-//					along with 16 bits of data input.  A program counter and instruction
-//					register is made as well, along with a 2-to-1 mux.  Lastly, a sign
-//					extension statement is made to extend an 8-bit signed value to a
-//					16-bit signed value, which will be added to a program counted to
-//					be put back into the program counter.
+// Purpose:	This is the CPU Execution Unit.  The module utilizes an integer
+//		datapath to execute instructions and write out to memory.  The 
+//		module outputs a 16-bit address location and 16 bits of data out, 
+//		along with 16 bits of data input.  A program counter and instruction
+//		register is made as well, along with a 2-to-1 mux.  Lastly, a sign
+//		extension statement is made to extend an 8-bit signed value to a
+//		16-bit signed value, which will be added to a program counted to
+//		be put back into the program counter.
 //
 //////////////////////////////////////////////////////////////////////////////////
 
 module CPU_EU(
-    input         clk,
-    input         rst,
-    input         adr_sel,
-	 input			pc_sel,
-    input         s_sel,
-    input         pc_ld,
-    input         pc_inc,
-    input         reg_w_en,
-    input         ir_ld,
-	 input	[2:0] W_Adr,
-	 input	[2:0] R_Adr,
-	 input	[2:0] S_Adr,
-	 input	[3:0] Alu_Op,
-    input  [15:0] D_in,
-    output [15:0] adr,
-	 output			N,
-	 output			Z,
-	 output			C,
+    input              clk,
+    input              rst,
+    input              adr_sel,
+    input	       pc_sel,
+    input              s_sel,
+    input              pc_ld,
+    input              pc_inc,
+    input              reg_w_en,
+    input              ir_ld,
+    input	[2:0]  W_Adr,
+    input	[2:0]  R_Adr,
+    input	[2:0]  S_Adr,
+    input	[3:0]  Alu_Op,
+    input  	[15:0] D_in,
+    output 	[15:0] adr,
+    output	       N,
+    output	       Z,
+    output	       C,
     output wire	[15:0] D_out, 
-	 output wire	[15:0] ir_out
+    output wire	[15:0] ir_out
     );
     
     wire [15:0] reg_out, pc, pc_mux_out, sign_ext, sign_ext_add;
     
-    IR			 			 ir(clk,
+    IR			 ir(clk,
                             rst,
                             ir_ld,
                             D_in,
                             ir_out);
                             
-    int_datapath         IntDP(clk, 
+    int_datapath      IntDP(clk, 
                             rst, 
                             W_Adr,
                             reg_w_en,
@@ -63,7 +62,7 @@ module CPU_EU(
                             Z,
                             C);
                       
-    PC						 ProgramCounter(clk,
+    PC	     ProgramCounter(clk,
                             rst,
                             pc_ld,
                             pc_inc,
